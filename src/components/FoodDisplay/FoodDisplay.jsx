d28efd93bca119e83d6da04354362e43674e1ade
Totan Sau <totansau7@gmail.com>
2024-12-05 21:02:19 +0530

    style changed

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -84,7 +84,7 @@ const FoodDisplay = ({ searchQuery = '' }) => {
           ) : (
             <div className="no-products-container">
               <div className="no-products-message">
-                <h2>No Foods Found</h2>
+                <h3 className="no-products-header">No Foods Found</h3>
                 <p>Sorry, we couldn't find any food that match your search. Please try again later or adjust your search criteria.</p>
               </div>
             </div>
