e9259fb986ab71289e87c29f776979fdd4d01a8a
Sekh Hasim Ahmed <hasim.cs@gmail.com>
2024-12-07 22:05:05 +0530

    Indentation fix.

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -5,14 +5,25 @@ import { StoreContext } from "../../context/StoreContext";
 import PropTypes from "prop-types";
 import axiosInstance from "../../common/axiosInstance";
 import { toast } from "react-toastify";
-import { useSearchParams } from "react-router-dom";
 
 const FoodItem = ({ id, name, price, description, image, ratings }) => {
-  const { API_BASE_URL, cartItems, addToCart, removeFromCart, authUser, token, setShowLogin, fetchFoodList, currentFetchFoodUrl } = useContext(StoreContext);  
+  const {
+    API_BASE_URL,
+    cartItems,
+    addToCart,
+    removeFromCart,
+    authUser,
+    token,
+    setShowLogin,
+    fetchFoodList,
+    currentFetchFoodUrl,
+  } = useContext(StoreContext);
 
-  let totalRating = !ratings.length ? 0 : ratings.reduce((acc, newVal) => acc + newVal.rating, 0)
-  let averageRating = !totalRating ? 0 : (totalRating / ratings.length)
-  
+  let totalRating = !ratings.length
+    ? 0
+    : ratings.reduce((acc, newVal) => acc + newVal.rating, 0);
+  let averageRating = !totalRating ? 0 : totalRating / ratings.length;
+
   const handleSubmitRating = async (rating, foodId) => {
     if (!token) {
       setShowLogin(true);
@@ -20,26 +31,32 @@ const FoodItem = ({ id, name, price, description, image, ratings }) => {
       return false;
     }
     const newReview = {
-        rating,
-        foodId,
-        userId: authUser?.userId
-    }
+      rating,
+      foodId,
+      userId: authUser?.userId,
+    };
 
-    await axiosInstance.post(
-      `${API_BASE_URL}api/user/give-rating/${authUser.userId}`,
-      newReview
-    )
-    .then(response => {
+    await axiosInstance
+      .post(`${API_BASE_URL}api/user/give-rating/${authUser.userId}`, newReview)
+      .then((response) => {
         if (response.data.success) {
           toast.success(response.data.message);
-          fetchFoodList(currentFetchFoodUrl.page, currentFetchFoodUrl.limit, currentFetchFoodUrl.search, currentFetchFoodUrl.category, currentFetchFoodUrl.priceShort);
-        } else { toast.error(response.data.message); }
-    })
-    .catch((err) => {      
-      toast.error(err.response.data.message);
-    });
-  }
-  
+          fetchFoodList(
+            currentFetchFoodUrl.page,
+            currentFetchFoodUrl.limit,
+            currentFetchFoodUrl.search,
+            currentFetchFoodUrl.category,
+            currentFetchFoodUrl.priceShort
+          );
+        } else {
+          toast.error(response.data.message);
+        }
+      })
+      .catch((err) => {
+        toast.error(err.response.data.message);
+      });
+  };
+
   return (
     <div className="food-item">
       <div className="food-item-image-container">
@@ -75,12 +92,25 @@ const FoodItem = ({ id, name, price, description, image, ratings }) => {
         <div className="food-item-name-rating">
           <p>{name}</p>
           <div className="star-rating">
-          {
-            [1, 2, 3, 4, 5].map(item => (
-              item <= averageRating ? <span className="star" key={item} onClick={() => handleSubmitRating(item, id)}>&#9733;</span> : <span className="star" key={item} onClick={() => handleSubmitRating(item, id)}>&#9734;</span>
+            {[1, 2, 3, 4, 5].map((item) =>
+              item <= averageRating ? (
+                <span
+                  className="star"
+                  key={item}
+                  onClick={() => handleSubmitRating(item, id)}
+                >
+                  &#9733;
+                </span>
+              ) : (
+                <span
+                  className="star"
+                  key={item}
+                  onClick={() => handleSubmitRating(item, id)}
+                >
+                  &#9734;
+                </span>
               )
-            )
-            }
+            )}
           </div>
         </div>
         <p className="food-item-description">{description}</p>
